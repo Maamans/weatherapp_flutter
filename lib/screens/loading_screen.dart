bc88c8f7bf47503d1cdd881efde7cb83016010c0
maamans <mansaray13416@gmail.com>
2023-05-18 12:56:56 +0000

    weatherapp flutter

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:weatheer/screens/location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:weatheer/services/weather.dart';
+
+
+
+
+
+
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+   
+  
+  void initState(){
+    super.initState();
+    
+    print('this line of code is triggered');
+     getLocationData();
+
+   
+    
+    
+  }
+   getLocationData()async{
+    
+    var weatherData = await WeatherModel().getLocationWeather();
+   
+
+    Navigator.push(context, MaterialPageRoute(builder: (context){
+      return LocationScreen(locationWeather:  weatherData,);
+    }));
+
+   
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+   
+    
+    return Scaffold(
+      
+      body: Center(
+        child: SpinKitCircle(
+          color: Colors.white,
+          size: 100,
+        ),
+      ),
+    );
+  }
+}
